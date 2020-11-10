86b16f9dcbc94ec8ff42600a9683e24fb3d85492
Ramya.U <ramyayadav2016@gmail.com>
2020-11-11 00:49:52 +0530

    [Ramya] Add . Added method to edit contact

---
+++ Day-20-AddressBook/AddressBook.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_20_AddressBook
+{
+    class AddressBook : IContact
+    {
+        //creating a dictionary for addressbook 
+        private Dictionary<string, Contact> addressBook = new Dictionary<string, Contact>();
+
+        /// <summary>
+        /// Adds the contact.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="address">The address.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="email">The email.</param>
+        /// <param name="zip">The zip.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
+        {
+            Contact contact = new Contact();
+            contact.FirstName = firstName;
+            contact.LastName = lastName;
+            contact.Address = address;
+            contact.City = city;
+            contact.State = state;
+            contact.Email = email;
+            contact.Zip = zip;
+            contact.PhoneNumber = phoneNumber;
+            addressBook.Add(contact.FirstName, contact);
+        }
+
+        /// <summary>
+        /// Views the contact.
+        /// </summary>
+        //method to display contact
+        public void ViewContact()
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBook)
+            {
+                Console.WriteLine("First Name : " + item.Value.FirstName);
+                Console.WriteLine("Last Name : " + item.Value.LastName);
+                Console.WriteLine("Address : " + item.Value.Address);
+                Console.WriteLine("City : " + item.Value.City);
+                Console.WriteLine("State : " + item.Value.State);
+                Console.WriteLine("Email : " + item.Value.Email);
+                Console.WriteLine("Zip : " + item.Value.Zip);
+                Console.WriteLine("Phone Number : " + item.Value.PhoneNumber + "\n");
+            }
+        }
+
+        /// <summary>
+        /// Edits the contact.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public void EditContact(string name)
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBook)
+            {
+                if (item.Key.Equals(name))
+                {
+                    Console.WriteLine("What do you want to edit ?? \n1.First Name \n2.Last Name \n3.Address \n4.City \n5.State \n6.Email \n7.Zip \n8.Phone Number");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter New First Name :");
+                            item.Value.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter New Last Name :");
+                            item.Value.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter New Address :");
+                            item.Value.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter New City :");
+                            item.Value.City = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter New State :");
+                            item.Value.State = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter New Email :");
+                            item.Value.Email = Console.ReadLine();
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter New Zip :");
+                            item.Value.Zip = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter New Phone Number :");
+                            item.Value.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
