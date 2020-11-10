9b36e2d75edad742434c5461ea10246ebe69352d
Ramya.U <ramyayadav2016@gmail.com>
2020-11-11 01:33:33 +0530

    [Ramya] Refactor .Modified Contact.cs

--- Day-20-AddressBook/Program.cs
+++ Day-20-AddressBook/Program.cs
@@ -11,12 +11,6 @@ namespace Day_20_AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Address Book");
-            //reference for addressbook class
-            AddressBook addressBook = new AddressBook();
-            //contact details
-            addressBook.AddContact("Ramya", "U", "Indiranagar", "Bangalore", "Karnataka", "ramya@gmail.com", 567890, 9988774455);
-            //for displaying contact
-            addressBook.ViewContact();
         }
     }
 }
