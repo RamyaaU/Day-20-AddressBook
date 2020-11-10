3150e9e19d2d33a1c472c22e7792bc0ac54d3a79
Ramya.U <ramyayadav2016@gmail.com>
2020-11-11 02:24:13 +0530

    [Ramya] Add . Added method to add multiple address

---
+++ Day-20-AddressBook/AddressBook.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_20_AddressBook
+{
+    class AddressBook
+    {
+        //creates a list
+        public List<Contact> People;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressBook"/> class.
+        /// </summary>
+        public AddressBook()
+        {
+            People = new List<Contact>();
+        }
+
+        /// <summary>
+        /// Finds the contact.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        /// <returns></returns>
+        public Contact FindContact(string fname)
+        {
+            Contact contact = People.Find((person) => person.FirstName == fname);
+            return contact;
+        }
+
+        /// <summary>
+        /// Adds the contact.
+        /// </summary>
+        /// <param name="FirstName">The first name.</param>
+        /// <param name="LastName">The last name.</param>
+        /// <param name="Address">The address.</param>
+        /// <param name="City">The city.</param>
+        /// <param name="State">The state.</param>
+        /// <param name="ZipCode">The zip code.</param>
+        /// <param name="PhoneNumber">The phone number.</param>
+        /// <param name="Email">The email.</param>
+        /// <returns></returns>
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            Contact result = FindContact(FirstName);
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Removes the contact.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool RemoveContact(string name)
+        {
+            Contact c = FindContact(name);
+
+            if (c != null)
+            {
+                People.Remove(c);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Day-20-AddressBook/AddressBookBinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_20_AddressBook
+{
+    class AddressBookBinder
+    {
+        //creating a dictionary by passing string and list as arguements
+        public Dictionary<string, List<Contact>> Binder = new Dictionary<string, List<Contact>>();
+
+        /// <summary>
+        /// Adds the addr book.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="list">The list.</param>
+        /// <returns></returns>
+        public List<Contact> AddAddrBook(string key, List<Contact> list)
+        {
+            if (this.Binder.ContainsKey(key))
+            {
+                Console.WriteLine("Address book already exists");
+                return Binder[key];
+            }
+            else
+            {
+                Console.WriteLine("New address book created");
+                Binder.Add(key, list);
+                return Binder[key];
+            }
+        }
+    }
+}
