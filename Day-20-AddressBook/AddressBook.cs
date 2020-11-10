56a5c8cc2a3c898d6f5abd1bbf760648c30a275c
Ramya.U <ramyayadav2016@gmail.com>
2020-11-11 03:22:25 +0530

    [Ramya] Add . Added method to sort by zip , state or city

---
+++ Day-20-AddressBook/AddressBook.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_20_AddressBook
+{
+    class AddressBook
+    {
+        //declaration
+        public List<Contact> People;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressBook"/> class.
+        /// </summary>
+        public AddressBook()
+        {
+            People = new List<Contact>();
+        }
+
+        /// <summary>
+        /// Finds the contact.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        /// <returns></returns>
+        public Contact FindContact(string fname)
+        {
+            Contact contact = null;
+            foreach (var person in People)
+            {
+                if (person.FirstName.Equals(fname))
+                {
+                    contact = person;
+                    break;
+                }
+            }
+            return contact;
+        }
+
+        /// <summary>
+        /// Adds the contact.
+        /// </summary>
+        /// <param name="FirstName">The first name.</param>
+        /// <param name="LastName">The last name.</param>
+        /// <param name="Address">The address.</param>
+        /// <param name="City">The city.</param>
+        /// <param name="State">The state.</param>
+        /// <param name="ZipCode">The zip code.</param>
+        /// <param name="PhoneNumber">The phone number.</param>
+        /// <param name="Email">The email.</param>
+        /// <returns></returns>
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            Contact result = FindContact(FirstName);
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Removes the contact.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool RemoveContact(string name)
+        {
+            Contact c = FindContact(name);
+
+            if (c != null)
+            {
+                People.Remove(c);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Alphabeticallies the arrange.
+        /// </summary>
+        public void AlphabeticallyArrange()
+        {
+            List<string> alphabeticalList = new List<string>();
+            foreach (Contact c in People)
+            {
+                string sort = c.ToString();
+                alphabeticalList.Add(sort);
+            }
+            alphabeticalList.Sort();
+            foreach (string s in alphabeticalList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        /// <summary>
+        /// Sorts the by pincode.
+        /// </summary>
+        public void SortByPincode()
+        {
+            People.Sort(new Comparison<Contact>((x, y) => string.Compare(x.ZipCode, y.ZipCode)));
+            foreach (Contact c in People)
+            {
+                Console.WriteLine(c.FirstName + "\t" + c.LastName + "\t" + c.Address + "\t" + c.City + "\t" + c.State + "\t" + c.ZipCode + "\t" + c.PhoneNumber + "\t" + c.Email);
+            }
+
+        }
+
+        /// <summary>
+        /// Sorts the by city.
+        /// </summary>
+        public void SortByCity()
+        {
+            People.Sort(new Comparison<Contact>((x, y) => string.Compare(x.City, y.City)));
+            foreach (Contact c in People)
+            {
+                Console.WriteLine(c.FirstName + "\t" + c.LastName + "\t" + c.Address + "\t" + c.City + "\t" + c.State + "\t" + c.ZipCode + "\t" + c.PhoneNumber + "\t" + c.Email);
+            }
+
+        }
+
+        /// <summary>
+        /// Sorts the state 
+        /// </summary>
+        public void SortByState()
+        {
+            People.Sort(new Comparison<Contact>((x, y) => string.Compare(x.State, y.State)));
+            foreach (Contact c in People)
+            {
+                Console.WriteLine(c.FirstName + "\t" + c.LastName + "\t" + c.Address + "\t" + c.City + "\t" + c.State + "\t" + c.ZipCode + "\t" + c.PhoneNumber + "\t" + c.Email);
+            }
+
+        }
+    }
+}
+
