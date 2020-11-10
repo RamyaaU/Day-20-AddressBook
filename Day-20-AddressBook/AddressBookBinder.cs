911f2f92a76d91f490bbf0e1091109dd8572a507
Ramya.U <ramyayadav2016@gmail.com>
2020-11-11 02:34:00 +0530

    [Ramya] Add . Added method to check no duplicate entry

---
+++ Day-20-AddressBook/AddressBookBinder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_20_AddressBook
+{
+    class AddressBookBinder
+    {
+        //creating dictionary to store contacts passing hashset as an arguement
+        public Dictionary<string, HashSet<Contact>> Binder = new Dictionary<string, HashSet<Contact>>();
+
+        /// <summary>
+        /// Adds the addr book.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="set">The set.</param>
+        /// <returns></returns>
+        public HashSet<Contact> AddAddrBook(string key, HashSet<Contact> set)
+        {
+            if (this.Binder.ContainsKey(key))
+            {
+                Console.WriteLine("Address book already exists");
+                return Binder[key];
+            }
+            else
+            {
+                Console.WriteLine("New address book created");
+                Binder.Add(key, set);
+                return Binder[key];
+            }
+        }
+    }
+}
+    
+
